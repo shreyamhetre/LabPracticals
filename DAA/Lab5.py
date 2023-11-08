c9b362f61948f30d6945d8d606ac140869a11878
shreyamhetre <112412167+shreyamhetre@users.noreply.github.com>
2023-11-08 08:15:33 +0530

    Create Lab5.py

---
+++ DAA/Lab5.py
@@ -0,0 +1,44 @@
+def solveNQueens(n: int, first_queen_col: int):
+    col = set()
+    posDiag = set()
+    negDiag = set()
+
+    res = []
+    board = [["."] * n for _ in range(n)]
+
+    def backtrack(r):
+        if r == n:
+            res.append(["".join(row) for row in board])
+            return
+
+        for c in range(n):
+            if c in col or (r + c) in posDiag or (r - c) in negDiag:
+                continue
+
+            col.add(c)
+            posDiag.add(r + c)
+            negDiag.add(r - c)
+            board[r][c] = "Q"
+
+            backtrack(r + 1)
+
+            col.remove(c)
+            posDiag.remove(r + c)
+            negDiag.remove(r - c)
+            board[r][c] = "."
+
+    col.add(first_queen_col)
+    posDiag.add(0 + first_queen_col)
+    negDiag.add(0 - first_queen_col)
+    board[0][first_queen_col] = "Q"
+
+    backtrack(1)  # Start with the second row
+    return res
+
+
+if __name__ == "__main__":
+    n = 8
+    first_queen_col = int(input("ENter pos of 1st queen"))
+    board = solveNQueens(n, first_queen_col)[0]
+    for row in board:
+        print(" ".join(row))
